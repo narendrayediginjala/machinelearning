2626f07eab55f7ae90c9e611db11ed5f8334b72d
Narendra Reddy Yediginjala <narendra.yediginjala@gmail.com>
2019-10-04 13:55:22 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,20 @@ pip3 install quandl
 pip3 install sklearn
 pip3 install pandas
 pip3 install matplotlib
+
+wget http://apache.claz.org/zookeeper/zookeeper-3.4.14/zookeeper-3.4.14.tar.gz
+mkdir zookeeper/
+tar -zxvf zookeeper-3.4.14.tar.gz -C ./zookeeper/
+
+cd ./zookeeper/zookeeper*
+cp ./conf/zoo_sample.cfg ./conf/zoo.cfg
+./bin/zkServer.sh start
+
+cd ../..
+
+wget http://apache.cs.utah.edu/kafka/2.3.0/kafka_2.12-2.3.0.tgz
+mkdir kafka
+tar -zxvf kafka_2.12-2.3.0.tgz -C ./kafka/
+
+cd kafka
+cd kafka*
